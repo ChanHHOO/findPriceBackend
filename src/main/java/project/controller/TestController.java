e74df3d42ead6346f7d14058b41573dedc27816f
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-06-13 14:50:15 +0000

    do not build

--- src/main/java/project/controller/TestController.java
+++ src/main/java/project/controller/TestController.java
@@ -1,5 +1,5 @@
 //https://sundries-in-myidea.tistory.com/71
-//https://ohjongsung.io/2020/07/18/spring-boot-with-react-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0
+
 //https://goddaehee.tistory.com/203?category=367461
 package project.controller;
 
@@ -14,11 +14,6 @@ import java.util.Date;
 
 @RestController
 public class TestController {
-    
-    @RequestMapping("/")
-	public String index() {
-		return "aa";
-	}
 
     @CrossOrigin(origins="*")
     @RequestMapping(value="/hello", method=RequestMethod.GET)
