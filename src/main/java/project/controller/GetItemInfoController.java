0b20a5338a17081ac3eb6d94a5227550005e5383
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-09-02 22:12:05 +0900

    realation with VO build

--- src/main/java/project/controller/GetItemInfoController.java
+++ src/main/java/project/controller/GetItemInfoController.java
@@ -2,6 +2,8 @@
 
 package project.controller;
 
+import project.vo.*;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,20 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
 public class GetItemInfoController {
-	
-	//proxy fix
-	@CrossOrigin(origins="*")
-    
-	@RequestMapping(method=RequestMethod.POST, path="/getDaangnData")
-	public String getDanngnData(@RequestBody String itemName) {
-		System.out.println(itemName);
-		return "bb";
-	}
 
+	// proxy fix
+	@CrossOrigin(origins="*", allowedHeaders = "*")
+	@ResponseBody
+	@RequestMapping(method=RequestMethod.POST, value="/getDaangnData")
+	public DaangnVO postRequest(@RequestBody DaangnVO daangnObj){
+		System.out.println(daangnObj.getItemTitle());
+		return daangnObj;
+	}
 }
