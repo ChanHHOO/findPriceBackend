9651b43b692744acb3f77e17a4a48cfdf0444187
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-09-02 22:12:55 +0900

    add data about daangn

--- src/main/java/project/vo/DaangnVO.java
+++ src/main/java/project/vo/DaangnVO.java
@@ -4,9 +4,27 @@ import lombok.*;
 
 @Getter
 @Setter
+public
 class DaangnVO{
     private String itemTitle;
     private String itemPrice;
+
+    public String getItemTitle() {
+        return this.itemTitle;
+    }
+
+    public void setItemTitle(String itemTitle) {
+        this.itemTitle = itemTitle;
+    }
+
+    public String getItemPrice() {
+        return this.itemPrice;
+    }
+
+    public void setItemPrice(String itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
 
     // 데이터 가공 코드 첨가 
 }
